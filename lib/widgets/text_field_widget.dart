66e2a68dc6467503b4dcce4319ad2f340228b48a
Nijat Naghiyev <nicat614@icloud.com>
2023-08-05 15:06:52 +0400

    Text Field Widget

---
+++ lib/widgets/text_field_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+        child: TextField(
+          decoration: InputDecoration(
+            enabledBorder: const UnderlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(
+                  14,
+                ),
+              ),
+            ),
+            filled: true,
+            hintText: 'Search News',
+            prefixIcon: Icon(
+              Icons.search,
+              color: isTextFieldShow ? Colors.transparent : Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
